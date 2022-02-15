f7bf8d06af6ee13d4117ca5c0b28f5d6e91cb444
funcys <ledhouse2077@gmail.com>
2022-02-15 22:43:48 +0900

    新規コミット

---
+++ src/ren_2_1/Main.java
@@ -0,0 +1,12 @@
+package ren_2_1;
+
+public class Main {
+	public static void main(String[] args) {
+		int x = 5;
+		int y = 10;
+		String ans = "x+yは" + (x + y);
+		System.out.println(ans);
+
+	}
+
+}
---
+++ src/ren_2_3/Main.java
@@ -0,0 +1,11 @@
+package ren_2_3;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("ようこそ占いの館へ");
+		System.out.println("あなたの名前を入力してください");
+
+
+	}
+
+}
